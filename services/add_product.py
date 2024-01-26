9c2d102db196ded3672a4b956aa99f8a99c06b4a
Bestbest2546 <bst12546@gmail.com>
2024-01-26 18:43:09 +0700

    upate

--- services/add_product.py
+++ services/add_product.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from module.database import users_collection
 from basemodel.product_id import ProductList
+from pymongo.errors import BulkWriteError
+from pymongo import UpdateOne
 
 router = APIRouter(
     tags=["Product Management"],
@@ -9,23 +11,22 @@ router = APIRouter(
 
 @router.post("/add_product/")
 async def add_product(list_product: ProductList):
-    try:
-        for product in list_product.list_product:
-            # Check if product_id already exists
-            if users_collection.find_one({"product_id": product.product_id}):
-                raise HTTPException(status_code=400, detail=f"Product with ID {product.product_id} already exists")
+    operations = []
+    existing_product_ids = set()
 
-            # Update the existing document by pushing the new product_id to list_product
-            update_result = users_collection.update_one(
-                {},
-                {"$push": {"list_product": {"product_id": product.product_id}}}
-            )
-            
-            # If the update did not match any documents, insert the first product_id
-            if update_result.matched_count == 0:
-                users_collection.insert_one({"list_product": [{"product_id": product.product_id}]})
-        
-        return {"message": "Products added successfully"}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    for product in list_product.list_product:
+        if product.product_id in existing_product_ids or users_collection.find_one({"list_product.product_id": product.product_id}):
+            raise HTTPException(status_code=400, detail=f"Product with ID {product.product_id} already exists")
+
+        existing_product_ids.add(product.product_id)
+        operations.append(UpdateOne({"list_product.product_id": {"$ne": product.product_id}},
+                                    {"$push": {"list_product": {"product_id": product.product_id}}},
+                                    upsert=True))
+
+    if operations:
+        try:
+            users_collection.bulk_write(operations)
+        except BulkWriteError as e:
+            raise HTTPException(status_code=500, detail=str(e.details))
+
+    return {"message": "Products added successfully"}
